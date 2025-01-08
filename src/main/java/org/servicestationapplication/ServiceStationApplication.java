53d2adff57a17a3c93a919f30458438a04de7ece
stackback <vshulenko@gmail.com>
2025-01-08 02:20:36 +0200

    init

---
+++ src/main/java/org/servicestationapplication/ServiceStationApplication.java
@@ -0,0 +1,13 @@
+package org.servicestationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceStationApplication.class, args);
+    }
+
+}
